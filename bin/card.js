7628b15883e467022aaf566f5da95d6c688c7dc7
unknown <t.khattara@yahoo.com>
2018-12-21 20:57:04 +0100

    Customizing and adding more labels

--- bin/card.js
+++ bin/card.js
@@ -14,33 +14,37 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
+  name: chalk.white('Toufik Khattara /'),
+  handle: chalk.cyan('Two Fik'),
+  work: chalk.white('Freelance Full Stack Web Developer'),
+  student: chalk.white('Computer Science'),
+  community: chalk.white('Founder/Organizer @GDGGhardaia'),
+  twitter: chalk.cyan('https://twitter.com/toufikimk'),
+  github: chalk.cyan('https://github.com/toufikimk'),
+  linkedin: chalk.cyan('https://linkedin.com/in/toufik-khattara'),
+  npx: chalk.white('npx twofik'),
+  labelWork: chalk.white.bold('    Work:'),
+  labelStudies: chalk.white.bold(' Studies:'),
+  labelCommunity: chalk.white.bold('Community:'),
+  labelTwitter: chalk.white.bold('  Twitter:'),
+  labelGitHub: chalk.white.bold('   GitHub:'),
+  labelLinkedIn: chalk.white.bold(' LinkedIn:'),
+  labelCard: chalk.white.bold(' Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
+const studying = `${data.labelStudies}  ${data.student}`
+const volunteering = `${data.labelCommunity}  ${data.community}`
+
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working +newline+ studying + newline+ volunteering+  newline + twittering + newline + githubing + newline + linkedining + newline + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
